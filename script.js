c1708801178095d0755ebc3bbde0d1676377e31d
tushareshukla <123384228+tushareshukla@users.noreply.github.com>
2023-12-10 05:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//function functionName() {
+ // alert(arguments.callee.name);
+//}
+
+// Example usage:
+//functionName(); // This will display an alert with the text "functionName"
+
+
+function functionName(){
+	alert(arguments.callee.name);
+}
+//example usage;
+functionName();//Thiswl
